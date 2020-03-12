d2336263eed24d475abf82bc286d64c513f6f22b
CoolCmd <CoolCmd@gmail.com>
2020-03-12 13:47:41 +0300

    for

--- for/crx.js
+++ for/crx.js
@@ -3,12 +3,9 @@
 var EXTENSION_ID = 'onlakpaheenokdbidfbaelffmaphgedp';
 
 var asBrowserVersion = navigator.userAgent.match(/Chrome\/(\d+)/);
-if (asBrowserVersion)
-{
-	var sFileURL = 'https://clients2.google.com/service/update2/crx?response=redirect&prod=chromiumcrx&prodversion='
-		+ encodeURIComponent(asBrowserVersion[1])
-		+ '&x='
-		+ encodeURIComponent('id=' + EXTENSION_ID + '&uc');
-	console.log(sFileURL);
-	location.replace(sFileURL);
-}
+var sFileUrl = 'https://clients2.google.com/service/update2/crx?response=redirect&prod=chromiumcrx&prodversion='
+	+ encodeURIComponent(asBrowserVersion[1])
+	+ '&x='
+	+ encodeURIComponent('id=' + EXTENSION_ID + '&uc');
+console.log(sFileUrl);
+location.replace(sFileUrl);
